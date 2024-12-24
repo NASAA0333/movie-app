470c316d543a194ea929f29e03d4794c079b69bb
NASAA0333 <nasanbuyan0333@gmail.com>
2024-12-24 20:18:18 +0800

    12-24 school

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import Image from "next/image";
+import { Footer } from "./Footer";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
