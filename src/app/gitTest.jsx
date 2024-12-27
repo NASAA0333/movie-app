a1389021d67ee4404fe09d3c87e5d921feb9bac4
NASAA0333 <nasanbuyan0333@gmail.com>
2024-12-27 21:11:21 +0800

    12

--- src/app/gitTest.jsx
+++
@@ -1,8 +0,0 @@
-export function homee (){
-    return <div> 
-        <div>git add .</div>
-    <div>git commit -m "12-27 home"</div>
-    <div>git push</div>
-    <div>git pull</div>
-    </div>
-}
